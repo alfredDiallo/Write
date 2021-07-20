01f5f5d48989a4612f161e10039bb4b8b4dec660
alfredDiallo <dialloalfred@gmail.com>
2021-07-20 02:10:30 +0000

    tester github

---
+++ write.js
@@ -0,0 +1,33 @@
+const target=document.getElementById('target');
+let array=['développeur','créatif','photographe'];
+let wordIndex=0;
+let letterIndex=0;
+const createLetter=()=>{
+    const letter=document.createElement('span');
+    target.appendChild(letter);
+    letter.textContent=array[wordIndex][letterIndex];
+    setTimeout(()=>{
+    letter.remove();},1000)
+}
+const loop=()=>{
+    setTimeout(()=>{
+    if(wordIndex>=array.length){
+        wordIndex=0;
+        letterIndex=0;
+        loop();
+    }
+    else if(letterIndex<array[wordIndex].length){ 
+        createLetter();
+        letterIndex++;
+        loop();
+    }
+    else{
+        wordIndex++;
+        letterIndex=0;
+       setTimeout(()=>{
+        loop(); 
+       },2000);
+       
+    }
+},50)} 
+loop();
